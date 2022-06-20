f7f1f1924cb16e92d7090974e70f3be635fd8910
tiko.otarashvili99@gmail.com <tiko.otarasvhili99@gmail.com>
2022-06-20 22:43:30 +0200

    flex slider

---
+++ myscript.js
@@ -0,0 +1,10 @@
+/*$(window).load(function() {
+    $('.flexslider').flexslider(); 
+  });// this function exists because of plugin latest verson doesn't support this
+
+*/
+
+
+$(window).on('load',function() {
+    $('.flexslider').flexslider(); 
+  });
